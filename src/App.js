24438c5ae4f996988fa9b0726b689faad442f18d
Naouel2 <tasbihate@gmail.com>
2024-05-21 10:43:24 +0200

    Add login & token verification for routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-/*pages*/
+// Pages
 import Login from './pages/Login';
 import Home from './pages/Home'
 import Formation from './pages/Formation';
+import Profile from './pages/Profile';
+
+// Components
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
-
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/formation" element={<Formation />} />
-    </Routes>
-
-  </Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/formation" element={<ProtectedRoute><Formation /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+      </Routes>
+    </Router>
   );
 }
 
