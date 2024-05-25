43ae891ed0520f3f4b61264352caecd7f6023da8
Naouel2 <tasbihate@gmail.com>
2024-05-25 17:47:09 +0200

    Add routes and CSS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Login from './pages/Login';
 import Home from './pages/Home'
 import Formation from './pages/Formation';
 import Profile from './pages/Profile';
+import Admin from './pages/Admin';
+import About from './pages/About';
+import CreateUser from './pages/CreateUser';
+import CreateFormation from './pages/Createformation';
 
 // Components
 import ProtectedRoute from './components/ProtectedRoute';
@@ -17,8 +21,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/formation" element={<ProtectedRoute><Formation /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
+        <Route path="/compte" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/formation/:id" element={<ProtectedRoute><Formation /></ProtectedRoute>} />
+        <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+        <Route path="/create-user" element={<ProtectedRoute><CreateUser /></ProtectedRoute>} />
+        <Route path="/create-formation" element={<ProtectedRoute><CreateFormation /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
